c6f65e35c38978788a5736e79d53b8d8a7da09f4
Omci <melloulchi.omar@gmail.com>
2023-03-19 00:39:56 +0100

    First experience with Discord Bot

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits, messageLink } = require('discord.js');
+require('dotenv/config')
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === 'ping') {
+    message.reply('pong')
+  }
+})
+client.login(process.env.TOKEN); // Replace with your Discord bot token
+
+
+
+
+
